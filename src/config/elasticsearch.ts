c1703ee09ac10b4c9552a9747e3ad53a0ebfa821
Firman Ardiansyah <ardiansyahfirman2004@gmail.com>
2025-06-25 21:35:16 +0700

    Update elasticsearch.ts

--- src/config/elasticsearch.ts
+++ src/config/elasticsearch.ts
@@ -1,6 +1,4 @@
 import { Client } from '@elastic/elasticsearch';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 export const esClient = new Client({
     node: process.env.ELASTICSEARCH_URL,
